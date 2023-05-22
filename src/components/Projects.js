ef6de128d792f9da65c4027be4a2922c6855569c
Prakhar Singh <92270107+intensifyprakhar@users.noreply.github.com>
2023-05-22 19:41:27 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,7 @@ import leetcode from "../assets/img/leetcode.png";
 import CodeChef from "../assets/img/codechef.png";
 import hackerrank from "../assets/img/hackerrank.png";
 import dbms from "../assets/img/dbms.png";
+import cn from "../assets/img/computerNetworks.png";
 import OS from "../assets/img/OS.png";
 
 import 'animate.css';
@@ -67,6 +68,11 @@ export const Projects = () => {
       description: "Operating System",
       imgUrl: OS,
     
+    },
+     {
+      description: "Coumputer Networks",
+      imgUrl: cn,
+      
     },
   ];
 
