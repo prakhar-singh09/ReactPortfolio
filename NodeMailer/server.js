4caace16cb32ca6554c2f9739144b2d19eab4d9e
Prakhar Singh <singhprakhar359@gmail.com>
2023-07-22 01:16:23 +0530

    NodeMailer added

---
+++ NodeMailer/server.js
@@ -0,0 +1,58 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+// server used to send send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(process.env.PORT || 5000, () => console.log("Server Running"));
+
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const name = `${firstName} ${lastName}`;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: process.env.EMAIL_USER,
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      res.status(500).json({ code: 500, status: "Failed to send message" });
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
